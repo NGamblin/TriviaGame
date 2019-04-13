3fff07f6a3c492b3f0e0a71b0dfd8c78c0daff7c
NGamblin <nickgamblin@gmail.com>
2019-04-13 15:12:50 -0500

    WIP

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+var qAndA = [{
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris",
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris",
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+},   {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+}];
+
+$("#start").on("click", function(){
+    console.log("Start!")
+    $("#start").remove();
+    $("#body").text(qAndA);
+});
+
+
+
+var qAndA = [{
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris",
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris",
+},  {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+},   {
+    question: "What is the capital of France?",
+    answerChoices:["Lisbon", "Beliz", "Porto", "Paris"],
+    correctChoice:"Paris", 
+}];
+
+
+
+
+
+// "what is the capital of Argentina?", "What is the capital of UAE?"
